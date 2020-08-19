8df8dc3af1d7cca785f00272672c692b29427bce
NOWOX67 <nowox.dev@gmail.com>
2020-08-19 23:09:16 +0200

    Started Health/Attack system
    Script PlayerAttack

---
+++ SaoGameProject/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public LayerMask playerLayerMask;
+    public float maxDistance;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, playerLayerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, Color.blue);
+            Debug.Log("Raycast touched a player");
+        }
+    }
+}
