de4ddbb3606561aa5e6f52728646de998c1f7a30
NOWOX67 <nowox.dev@gmail.com>
2020-08-20 23:58:14 +0200

    just things that have no importance

--- SaoGameProject/Assets/Scripts/PlayerAttack.cs
+++ SaoGameProject/Assets/Scripts/PlayerAttack.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public LayerMask playerLayerMask;
-    public float maxDistance;
-
-    void Start()
-    {
-
-    }
-
-    void FixedUpdate()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, playerLayerMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance, Color.blue);
-            Debug.Log("Raycast touched a player");
-        }
-    }
+   
 }
