874f2e35f2faf729c682ee78cfeb698edc0b7eae
ARKAGEDON <38785328+ARKAGEDON@users.noreply.github.com>
2020-08-21 12:13:01 +0200

    Ajout UI

---
+++ SaoGame/Assets/Scripts/SwapImage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SwapImage : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Images;
+    [SerializeField] private GameObject[] Texts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject active in Images)
+        {
+            active.SetActive(false);
+        }
+        foreach (GameObject active in Texts)
+        {
+            active.SetActive(false);
+        }
+        GameObject ImageToActive = Images[0];
+        ImageToActive.SetActive(true);
+        GameObject TextToActive = Texts[0];
+        TextToActive.SetActive(true);
+    }
+    public void Activate(int number)
+    {
+        foreach (GameObject active in Images)
+        {
+            active.SetActive(false);
+        }
+        foreach (GameObject active in Texts)
+        {
+            active.SetActive(false);
+        }
+        GameObject ImageToActive = Images[number];
+        ImageToActive.SetActive(true);
+        GameObject TextToActive = Texts[number];
+        TextToActive.SetActive(true);
+    }
+}
---
+++ SaoGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Tooltip("Mettre les actives que vous voulez dans l'array et puis sur le boutton mettre la fonction Activate ainsi que l'int de l'active (array commence par 0)")]
+    [SerializeField] private GameObject[] Activable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject active in Activable)
+        {
+            active.SetActive(false);
+        }
+    }
+
+    public void Activate(int number)
+    {
+        foreach (GameObject active in Activable)
+        {
+            active.SetActive(false);
+        }
+        GameObject ToActive = Activable[number];
+        ToActive.SetActive(true);
+    }
+}
