e65e2e5616b70fb2d0f0de3f6804fdb20089ebf0
NOWOX67 <nowox.dev@gmail.com>
2020-08-21 02:46:47 +0200

    Health & Attack done

---
+++ SaoGameProject/Assets/NowoxScene/Scripts/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float damage;
+    public float fireRate;
+    public float range;
+
+    public Transform playerHead;
+
+    private float currentFireRate;
+
+    private void Awake()
+    {
+        currentFireRate = fireRate;
+    }
+
+    private void Update()
+    {
+        currentFireRate += Time.deltaTime;
+
+        if(Input.GetMouseButtonDown(0) && currentFireRate >= fireRate)
+        {
+            currentFireRate = 0f;
+            Attack();
+        }
+    }
+
+    public void Attack()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(playerHead.position, playerHead.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name + " with the " + hit.transform.tag + " tag" + " has been hit");
+
+            AttackableGameObject target = hit.transform.GetComponent<AttackableGameObject>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ SaoGameProject/Assets/PreInstalledAssets/UIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Active de chaque boutton")]
+    [SerializeField] private GameObject ActiveMe;
+    [SerializeField] private GameObject ActiveFriends;
+    [SerializeField] private GameObject ActiveMessage;
+    [SerializeField] private GameObject ActiveLocation;
+    [SerializeField] private GameObject ActiveSettings;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActiveMe.SetActive(false);
+        ActiveFriends.SetActive(false);
+        ActiveMessage.SetActive(false);
+        ActiveLocation.SetActive(false);
+        ActiveSettings.SetActive(false);
+    }
+
+    //Active button pour Me
+    public void Me()
+    {
+        ActiveMe.SetActive(true);
+        ActiveFriends.SetActive(false);
+        ActiveMessage.SetActive(false);
+        ActiveLocation.SetActive(false);
+        ActiveSettings.SetActive(false);
+    }
+    //Active button pour Friends
+    public void Friends()
+    {
+        ActiveMe.SetActive(false);
+        ActiveFriends.SetActive(true);
+        ActiveMessage.SetActive(false);
+        ActiveLocation.SetActive(false);
+        ActiveSettings.SetActive(false);
+    }
+    //Active button pour Message
+    public void Message()
+    {
+        ActiveMe.SetActive(false);
+        ActiveFriends.SetActive(false);
+        ActiveMessage.SetActive(true);
+        ActiveLocation.SetActive(false);
+        ActiveSettings.SetActive(false);
+    }
+    //Active button pour Location
+    public void Location()
+    {
+        ActiveMe.SetActive(false);
+        ActiveFriends.SetActive(false);
+        ActiveMessage.SetActive(false);
+        ActiveLocation.SetActive(true);
+        ActiveSettings.SetActive(false);
+    }
+    //Active button pour Settings
+    public void Settings()
+    {
+        ActiveMe.SetActive(false);
+        ActiveFriends.SetActive(false);
+        ActiveMessage.SetActive(false);
+        ActiveLocation.SetActive(false);
+        ActiveSettings.SetActive(true);
+    }
+}
